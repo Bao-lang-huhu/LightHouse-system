cc60eff438812bd86e4721541061d57a10f36ffb
Bao-lang-huhu <146758379+Bao-lang-huhu@users.noreply.github.com>
2024-10-05 02:52:47 -0700

    Update index.js
    REMOVE  a "/"

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use(cors({
 
 // Handle preflight requests for all routes
 app.options('*', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://light-house-system-d6wn.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://light-house-system-d6wn.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');

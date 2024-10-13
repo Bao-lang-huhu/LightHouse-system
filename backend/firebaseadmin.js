e71ccef32e48ad439f680a8a1c84371ca9cb7675
bao-lang-huhu <eolhingpit@gmail.com>
2024-10-14 05:31:57 +0800

    new reg

--- backend/firebaseadmin.js
+++ backend/firebaseadmin.js
@@ -1,10 +1,13 @@
 const admin = require("firebase-admin");
 
+console.log("Private Key Before Replace:", process.env.FIREBASE_PRIVATE_KEY);
+console.log("Private Key After Replace:", process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'));
+
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
 });
 

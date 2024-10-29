49544dc0335731432db06a602f990598fddbc62e
Bao-lang-huhu <146758379+Bao-lang-huhu@users.noreply.github.com>
2024-10-29 10:14:29 +0800

    Update eventForecast.js

--- backend/routes/eventForecast.js
+++ backend/routes/eventForecast.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const axios = require('axios');
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = "https://cayfvgjakympxwknatco.supabase.co";
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNheWZ2Z2pha3ltcHh3a25hdGNvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMzc4MDI3MCwiZXhwIjoyMDM5MzU2MjcwfQ.Wr1jpEbcUhAhfoWz4bH2FYvlz8kIgIKEcDIK7mjGq78';
+const supabaseUrl = "https://qerqnluaffgbdvlkygpa.supabase.co";
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlcnFubHVhZmZnYmR2bGt5Z3BhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMDA1ODk1OCwiZXhwIjoyMDQ1NjM0OTU4fQ.MzOOYe1DktKd_3gQ8f-M5wyDCkIg_f_AQ8sHy88OXnA';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const flaskApiUrl = 'https://lavish-consideration-production.up.railway.app';

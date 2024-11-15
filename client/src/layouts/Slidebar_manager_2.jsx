dd9010967ad71b6c834204e6f138dc7378117a1e
chasteBlue <106545588+chasteBlue@users.noreply.github.com>
2024-11-15 14:24:22 +0800

    change the name to Order Reports -sidebar

--- client/src/layouts/Slidebar_manager_2.jsx
+++ client/src/layouts/Slidebar_manager_2.jsx
@@ -98,11 +98,11 @@ const SidebarManager2 = () => {
                 <li>
                     <Link 
                       to="/manager_report_menu_optimization" 
-                      title="Menu Suggestions"
+                      title="Order Reports"
                       className={location.pathname === '/manager_report_menu_optimization' ? 'is-right-active' : ''}
                     >
                     <IoFastFood style={{ marginRight: isSidebarOpen ? '5px' : '0', textAlign: 'center' }} />
-                    {isSidebarOpen && 'Menu Suggestions'}
+                    {isSidebarOpen && 'Order Reports'}
                     </Link>
                 </li>
                 <li>
